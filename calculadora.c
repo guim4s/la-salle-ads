d721a291ae02c5f8ffbad8a2219a4a43a0221374
Guilherme Wendt <guiminha.w@outlook.com>
2022-10-06 22:49:22 -0300

    Update calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,149 +1,155 @@
+/*
+* Autor: Guilherme Wendt
+* Versão: 1.0
+* GitHub: @guim4s
+* Link para o projeto: https://github.com/guim4s/la-salle-ads
+*/
+
 #include <stdbool.h>
 #include <math.h>
 #include <stdio.h>
 
-int main() {
+/* 
+* Função que obtem o primeiro valor para realizar as operações 
+* Optei em separar o código para poder ter uma validação sobre os valores menores que 0
+* E não ficar repetivivo dentro de cada operação
+*/
+float obtemNumero1()
+{
+  float numero1;
+  printf("\nDigite o 1o valor: ");
+  scanf("%f", &numero1);
 
-  float numero1, numero2, resultado;
-  int operacao;
-  bool sair;
-  
-  printf("\nBem vindo a Calculadora escrita em C\n");
-  
-  while (!sair) {
-    printf("Escolha a sua opção: \n");
+  while (numero1 <= 0)
+  {
+    printf("O valor digitado não pode ser igual ou menor que 0\n");
+    printf("Digite novamente: ");
+    scanf("%f", &numero1);
+  }
+  return numero1;
+}
+
+// Função que obtem o segundo valor para realizar as operações 
+float obtemNumero2()
+{
+  float numero2;
+  printf("Digite o 2o valor: ");
+  scanf("%f", &numero2);
+
+  while (numero2 <= 0)
+  {
+    printf("O valor digitado nao pode ser igual ou menor que 0\n");
+    printf("Digite novamente: ");
+    scanf("%f", &numero2);
+  }
+
+  return numero2;
+}
+
+int obtemPotencia()
+{
+  int potencia;
+  printf("Digite o valor para potencializar: ");
+  scanf("%d", &potencia);
+
+  while (potencia <= 0)
+  {
+    printf("O valor digitado nao pode ser igual ou menor que 0\n");
+    printf("Digite novamente: ");
+    scanf("%d", &potencia);
+  }
+
+  return potencia;
+}
+
+int main()
+{
+
+  // Inicializaçao das variáveis utilizadas 
+  float valor1, valor2, resultado;
+  int operacao, potencia;
+  bool sair = false;
+
+  // Mensadem inicial, conforme solicitada
+  printf("\nSou o Guilherme Wendt, curso Analise e Desenvolvimento de Sistemas na La Salle.\n");
+  printf("Codigo de aluno: 202210989\n");
+  printf("\nBem vindo a Calculadora escrita em C!\n");
+
+  /* 
+  * Menu de oções, permanece rodando durante toda a execução do programa
+  * Ele só é encerrado quando o usuário escolha a opção de saída
+  */
+  while (!sair)
+  {
+    printf("Escolha a sua opcao: \n");
     printf("1 - Soma\n");
-    printf("2 - Substração\n");
-    printf("3 - Divisão\n");
-    printf("4 - Multiplicação\n");
+    printf("2 - Substracao\n");
+    printf("3 - Divisao\n");
+    printf("4 - Multiplicacao\n");
     printf("5 - Raiz Quadrada\n");
     printf("6 - Potencia\n");
     printf("0 - Sair\n");
-    printf("Opção > ");
+    printf("Opcao > ");
     scanf("%d", &operacao);
-    
-    switch (operacao) {
-      case 0:
-        printf("\n\nVoce esta saindo da Calculadora!");
-        sair = true;
-        break;
 
-      case 1:
-        printf("Digite o numero 1: ");
-        scanf("%f", &numero1);
+    switch (operacao)
+    {
+    case 0:
+      printf("\nEncerrando as oepracoes...\n");
+      printf("Bye!\n");
+      sair = true;
+      break;
 
-        while (numero1 <= 0) {
-            printf("O valor digitado não pode ser igual ou menor que 0\n");
-            printf("Digite o numero 1 novamente: ");
-            scanf("%f", &numero1);  
-        }
+    case 1:
+      // Atribuição dos valores para realizar o cálculo
+      // Como o resultado é atribuído a outra variável, o próprio executável já faz a chamada para a função 
+      valor1 = obtemNumero1();
+      valor2 = obtemNumero2();
+      resultado = valor1 + valor2;
+      printf("%.0f + %.0f = %.2f\n\n", valor1, valor2, resultado);
+      break;
 
-        printf("Digite o numero 2: ");
-        scanf("%f", &numero2);
+    case 2:
+      valor1 = obtemNumero1();
+      valor2 = obtemNumero2();
+      resultado = valor1 - valor2;
 
-        while (numero2 <= 0) {
-            printf("O valor digitado não pode ser igual ou menor que 0\n");
-            printf("Digite o numero 1 novamente: ");
-            scanf("%f", &numero1);  
-        }  
+      printf("%.0f - %.0f = %.2f\n\n", valor1, valor2, resultado);
+      break;
 
-        resultado = numero1 + numero2;
-        printf("A soma entre os dois valores eh: %.2f\n\n", resultado);
-        break;
+    case 3:
+      valor1 = obtemNumero1();
+      valor2 = obtemNumero2();
+      resultado = valor1 / valor2;
+      printf("%.0f / %.0f = %.2f\n\n", valor1, valor2, resultado);
+      break;
 
-      case 2:
-        printf("Digite o numero 1: ");
-        scanf("%f", &numero1);
+    case 4:
+      valor1 = obtemNumero1();
+      valor2 = obtemNumero2();
 
-        while (numero1 <= 0) {
-            printf("O valor digitado não pode ser igual ou menor que 0\n");
-            printf("Digite o numero 1 novamente: ");
-            scanf("%f", &numero1);  
-        }
+      resultado = valor1 * valor2;
+      printf("%.0f x %.0f = %.2f\n\n", valor1, valor2, resultado);
+      break;
 
-        printf("Digite o numero 2: ");
-        scanf("%f", &numero2);
+    case 5:
+      valor1 = obtemNumero1();
+      resultado = sqrt(valor1);
+      printf("A raiz quadrada de %.0f = %.2f\n\n", valor1, resultado);
+      break;
 
-        while (numero2 <= 0) {
-            printf("O valor digitado não pode ser igual ou menor que 0\n");
-            printf("Digite o numero 1 novamente: ");
-            scanf("%f", &numero1);  
-        }  
-        
-        resultado = numero1 - numero2;
-        printf("A subtração entre os dois valores eh: %.2f\n\n", resultado);  
-        break;
+    case 6:
+      valor1 = obtemNumero1();
+      potencia = obtemPotencia();
+      resultado = powf(valor1, potencia);
+      printf("%.2f elevado a potencia de %d =  %.2f\n\n", valor1, potencia, resultado);
+      break;
 
-      case 3:
-        printf("Digite o numero 1: ");
-        scanf("%f", &numero1);
-
-        while (numero1 <= 0) {
-            printf("O valor digitado não pode ser igual ou menor que 0\n");
-            printf("Digite o numero 1 novamente: ");
-            scanf("%f", &numero1);  
-        }
-
-        printf("Digite o numero 2: ");
-        scanf("%f", &numero2);
-
-        while (numero2 <= 0) {
-            printf("O valor digitado não pode ser igual ou menor que 0\n");
-            printf("Digite o numero 1 novamente: ");
-            scanf("%f", &numero1);  
-        }  
-        
-        resultado = numero1 / numero2;
-        printf("A divisão entre os dois valores eh: %.2f\n\n", resultado);
-        break;
-
-      case 4: 
-        printf("Digite o numero 1: ");
-        scanf("%f", &numero1);
-
-        while (numero1 <= 0) {
-            printf("O valor digitado não pode ser igual ou menor que 0\n");
-            printf("Digite o numero 1 novamente: ");
-            scanf("%f", &numero1);  
-        }
-
-        printf("Digite o numero 2: ");
-        scanf("%f", &numero2);
-
-        while (numero2 <= 0) {
-            printf("O valor digitado não pode ser igual ou menor que 0\n");
-            printf("Digite o numero 1 novamente: ");
-            scanf("%f", &numero1);  
-        }  
-        
-        resultado = numero1 * numero2;
-        printf("A multiplicação entre os dois valores eh: %.2f\n\n", resultado);
-        break;
-
-      case 5:
-        printf("Digite o numero 1: ");
-        scanf("%f", &numero1);
-
-        while (numero1 <= 0) {
-            printf("O valor digitado não pode ser igual ou menor que 0\n");
-            printf("Digite o numero 1 novamente: ");
-            scanf("%f", &numero1);  
-        }
-
-        resultado = sqrt(numero1);
-        printf("A raiz quadrada eh: %.2f\n\n", resultado);
-        break;
-
-      case 6:
-        break;
-
-      default:
-        printf("\nCodigo invalido, tente novamente\n\n");
+    default:
+      printf("\nCodigo invalido, tente novamente.\n\n");
       break;
     }
-
   }
 
   return 0;
 }
-  
